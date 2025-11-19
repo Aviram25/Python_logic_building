a12a27d56d595234a114c6f6a5c8a1a9b0e1d6b8
Aviram Dhagat <dhagataviram@gmail.com>
2025-11-19 21:11:19 +0530

    Update Triangular_sum.py

--- DataLamurQuestions/Triangular_sum.py
+++ DataLamurQuestions/Triangular_sum.py
@@ -19,3 +19,14 @@ def triangular_sum_v1(nums):
     
     # Recursively process the new array
     return triangular_sum_v1(new_nums)
+
+
+# def triangular_sum_v2(nums):
+#   if len(nums) == 1:
+#     return nums[0]
+    
+#   new_nums = []
+#   for i in range(len(nums)-1):
+#     new_nums.append((nums[i]+ nums[i+1])%10 )
+    
+  # return triangular_sum_v2(new_nums)  
